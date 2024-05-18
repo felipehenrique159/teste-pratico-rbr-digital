d25ab6cc5f6279aca179007b25712c4e8a793de2
felipehenrique159 <felipecod6@hotmail.com>
2024-05-18 10:03:10 -0300

    feat: instalado cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import routes from './routes'
 import mongoose from 'mongoose'
+import cors from 'cors'
 
 const app = express();
 app.use(express.json())
+app.use(cors())
 const port = 3001;
 
 app.listen(port, () => {
